25a8c1828fcb8e9829c26e3107aaa7cc3f73585e
Lucie CULIN <culinlucie@gmail.com>
2022-12-07 15:16:41 +0100

    pages de jeux

---
+++ demarrer_le_jeu.php
@@ -0,0 +1,81 @@
+<div class="jeux">
+            <button class="btn" href="#">FILMS</button>
+            <div class="position">
+                <div class="cinquante-text">
+                    <ul>
+                        <li class="li"><a class="a" href='#'>Harry Potter</a></li>
+                        <li class="li"><a class="a" href='#'>Star Wars</a></li>
+                        <li class="li"><a class="a" href='#'>Le Seigneur Des Anneaux</a></li>
+                        <li class="li"><a class="a" href='#'>Films</a></li>
+                    </ul>
+                </div>
+                <div class="img-films">
+
+                </div>
+            </div>
+            <button class="btn" href="#">SERIES</button>
+            <div class="position">
+                <div class="cinquante-text">
+                    <ul>
+                        <li class="li"><a class="a" href='#'>Kaamelott</a></li>
+                        <li class="li"><a class="a" href='#'>Games Of Thrones</a></li>
+                        <li class="li"><a class="a" href='#'>Malcolm</a></li>
+                        <li class="li"><a class="a" href='#'>Séries</a></li>
+                    </ul>
+                </div>
+                <div class="img-series">
+
+                </div>
+            </div>
+            <button class="btn" href="#">DESSINS ANIMES</button>
+            <div class="position">
+                <div class="cinquante-text">
+                    <ul>
+                        <li class="li"><a class="a" href='#'>Disney</a></li>
+                        <li class="li"><a class="a" href='#'>Club Dorothée</a></li>
+                        <li class="li"><a class="a" href='#'>Warner Bros</a></li>
+                        <li class="li"><a class="a" href='#'>Dessins Animés</a></li>
+                    </ul>
+                </div>
+                <div class="img-dessinsAnimes"></div>
+            </div>
+            <button class="btn" href="#">ANIMES</button>
+            <div class="position">
+                <div class="cinquante-text">
+                    <ul>
+                        <li class="li"><a class="a" href='#'>One Piece</a></li>
+                        <li class="li"><a class="a" href='#'>Naruto</a></li>
+                        <li class="li"><a class="a" href='#'>Dragon Ball Z</a></li>
+                        <li class="li"><a class="a" href='#'>Animés</a></li>
+                    </ul>
+                </div>
+                <div class="img-animes"></div>
+            </div>
+            <button class="btn" href="#">JEUX VIDEO</button>
+            <div class="position">
+                <div class="cinquante-text">
+                    <ul>
+                        <li class="li"><a class="a" href='#'>Nintendo</a></li>
+                        <li class="li"><a class="a" href='#'>Jeux Rétro</a></li>
+                        <li class="li"><a class="a" href='#'>Jeux Indépendants</a></li>
+                        <li class="li"><a class="a" href='#'>Jeux Vidéo</a></li>
+                    </ul>
+                </div>
+                <div class="img-jeuxVideo"></div>
+            </div>
+            <button class="btn" href="#">CULTURE</button>
+            <div class="position">
+                <div class="cinquante-text">
+                    <ul>
+                        <li class="li"><a class="a" href='#'>Culture Pub</a></li>
+                        <li class="li"><a class="a" href='#'>Culture Musicale</a></li>
+                        <li class="li"><a class="a" href='#'>Culture Stars</a></li>
+                        <li class="li"><a class="a" href='#'>Culture</a></li>
+                    </ul>
+                </div>
+                <div class="img-culture"></div>
+            </div>
+
+
+        </div>
+    </div>
---
+++ les_jeux.php
@@ -0,0 +1,30 @@
+<?php
+include('./partials/header.php');
+include('./partials/nav.php');
+?>
+<link rel="stylesheet" href="./style_demarrer_le-jeux.css" />
+
+
+<div class="lesjeux">
+    <div class="demarrer">
+        <div class="pagination">
+            <div class="card-jeux">
+                <h1><a href='#'>DEMARRER LE JEUX</a></h1>
+                <div class="img-go"></div>
+            </div>
+            <div class="card-jeux">
+                <h1><a href='#'>DEFIE CONTRE LA MONTRE</a></h1>
+                <div class="img-time"></div>
+            </div>
+            <div class="card-jeux">
+                <h1><a href='#'>DEFIE TES AMIS</a></h1>
+                <div class="img-friends"></div>
+            </div>
+
+        </div>
+
+
+
+<?php
+include('./partials/footer.php');
+?>
