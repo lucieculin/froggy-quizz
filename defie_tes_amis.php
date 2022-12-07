968671d1e083b234a2fbf45040fdc761696c5319
Lucie CULIN <culinlucie@gmail.com>
2022-12-07 13:37:26 +0100

    Add defie amis php

---
+++ defie_tes_amis.php
@@ -0,0 +1,66 @@
+<?php
+include('./partials/header.php');
+include('./partials/nav.php');
+?>
+<link rel="stylesheet" href="./style_demarrer_le-jeux.css" />
+
+
+<div class="amis">
+        <h1>DEFIE TES AMIS</h1>
+        <div class="btn-amis">
+        <div>
+            <button type="button" class="btn-user">
+                Bertrand
+                <span class="user">
+                    <span class="online">En ligne</span>
+                </span>
+            </button>
+        </div>
+
+        <div>
+            <button type="button" class="btn-user">
+                Sylvain
+                <span class="user">
+                    <span class="online">En ligne</span>
+                </span>
+            </button>
+        </div>
+
+        <div>
+            <button type="button" class="btn-user">
+                Aurélie
+                <span class="user">
+                    <span class="online">Absent</span>
+                </span>
+            </button>
+        </div>
+
+
+        <div>
+            <button type="button" class="btn-user">
+                Capucine
+                <span class="user">
+                    <span class="online">Absent</span>
+                </span>
+            </button>
+        </div>
+
+        <div>
+            <button type="button" class="btn-user">
+                Utilisateur Aléatoire en ligne
+                <span class="user">
+                    <span class="online">en ligne</span>
+                </span>
+            </button>
+        </div>
+        </div>
+
+
+
+
+
+    </div>
+
+    <?php
+    include('./partials/footer.php');
+    ?>
