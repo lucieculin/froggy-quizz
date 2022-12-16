62a537b6817006292295b0c892ad37218c782be5
Lucie CULIN <culinlucie@gmail.com>
2022-12-16 17:09:38 +0100

    travail du 16122022

--- demarrer_le_jeu.php
+++ demarrer_le_jeu.php
@@ -7,78 +7,200 @@ include('./partials/header.php');
             <button class="btn" href="#">FILMS</button>
             <div class="position">
                 <div class="cinquante-text">
-                    <ul>
-                        <li class="li"><a class="a" href='#'>Harry Potter</a></li>
-                        <li class="li"><a class="a" href='#'>Star Wars</a></li>
-                        <li class="li"><a class="a" href='#'>Le Seigneur Des Anneaux</a></li>
-                        <li class="li"><a class="a" href='#'>Films</a></li>
+                    <ul class="ul">
+                        <div class="tab">
+                        <li class="li">
+                            <a class="a" id="harry-potter" href='#'>Harry Potter</a>
+                            <img class="img" src="./assets/photos/HarryPotter.jpg" alt="HarryPotter">
+                        </li>
+
+                        <li class="li">
+                            <a class="a" id="star-wars" href='#'>Star Wars</a>
+                            <img class="img" src="./assets/photos/starwars.jpg" alt="StarWars">
+                        </li>
+                        </div>
+                        <div class="tab">
+                        <li class="li">
+                            <a class="a" id="seigneur-des-anneaux" href='#'>Le Seigneur Des Anneaux</a>
+                            <img class="img" src="./assets/photos/seigneurdesanneaux.jpg" alt="SeigneurDesAnneaux">
+                        </li>
+
+                        <li class="li">
+                            <a class="a" href='#'>Films</a>
+                            <img class="img" src="./assets/photos/Films.jpg" alt="Films">
+                        </li>
+                        </div>
                     </ul>
                 </div>
-                <div class="img-films">
+
+                <div class="img-films" >
 
                 </div>
             </div>
+
             <button class="btn" href="#">SERIES</button>
             <div class="position">
                 <div class="cinquante-text">
-                    <ul>
-                        <li class="li"><a class="a" href='#'>Kaamelott</a></li>
-                        <li class="li"><a class="a" href='#'>Games Of Thrones</a></li>
-                        <li class="li"><a class="a" href='#'>Malcolm</a></li>
-                        <li class="li"><a class="a" href='#'>Séries</a></li>
+                    <ul class="ul">
+                        <div class="tab">
+                        <li class="li">
+                            <a class="a" id="kaamelott" href='#'>Kaamelott</a>
+                            <img class="img" src="./assets/photos/Kaamelott.jpg" alt="Kaamelott">
+                        </li>
+
+                        <li class="li">
+                            <a class="a" id="games-of-thrones" href='#'>Games Of Thrones</a>
+                            <img class="img" src="./assets/photos/gameOgThrone.jpg" alt="GamesOfThrone">
+                        </li>
+                        </div>
+                        <div class="tab">
+                        <li class="li">
+                            <a class="a" id="malcolm" href='#'>Malcolm</a>
+                            <img class="img" src="./assets/photos/Malcolm.jpg" alt="Malcolm">
+                        </li>
+
+                        <li class="li">
+                            <a class="a" href='#'>Séries</a>
+                            <img class="img" src="./assets/photos/series.jpg" alt="Series">
+                        </li>
+                        </div>
                     </ul>
                 </div>
-                <div class="img-series">
+                <div class="img-series" alt="imgSeries">
 
                 </div>
             </div>
             <button class="btn" href="#">DESSINS ANIMES</button>
             <div class="position">
                 <div class="cinquante-text">
-                    <ul>
-                        <li class="li"><a class="a" href='#'>Disney</a></li>
-                        <li class="li"><a class="a" href='#'>Club Dorothée</a></li>
-                        <li class="li"><a class="a" href='#'>Warner Bros</a></li>
-                        <li class="li"><a class="a" href='#'>Dessins Animés</a></li>
+                    <ul class="ul">
+                        <div class="tab">
+                        <li class="li">
+                            <a class="a" id="disney" href='#'>Disney</a>
+                            <img class="img" src="./assets/photos/Disney.jpg" alt="Disney">
+                        </li>
+
+                        <li class="li">
+                            <a class="a" id="club-dorothee" href='#'>Club Dorothée</a>
+                            <img class="img" src="./assets/photos/clubDorothee.jpg" alt="ClubDorothee">
+                        </li>
+                        </div>
+                        <div class="tab">
+                        <li class="li">
+                            <a class="a" id="warner-bross" href='#'>Warner Bros</a>
+                            <img class="img" src="./assets/photos/Warner%20Bros.jpg" alt="WarnerBross">
+                        </li>
+
+                        <li class="li">
+                            <a class="a" href='#'>Dessins Animés</a>
+                            <img class="img" src="./assets/photos/DessinsAnimes.jpg" alt="DessinsAnimes">
+                        </li>
+                        </div>
                     </ul>
+
                 </div>
-                <div class="img-dessinsAnimes"></div>
+
+                <div class="img-dessinsAnimes" alt="imgdessinsAnimes">
+
+                </div>
             </div>
             <button class="btn" href="#">ANIMES</button>
             <div class="position">
                 <div class="cinquante-text">
-                    <ul>
-                        <li class="li"><a class="a" href='#'>One Piece</a></li>
-                        <li class="li"><a class="a" href='#'>Naruto</a></li>
-                        <li class="li"><a class="a" href='#'>Dragon Ball Z</a></li>
-                        <li class="li"><a class="a" href='#'>Animés</a></li>
+                    <ul class="ul">
+                        <div class="tab">
+                        <li class="li">
+                            <a class="a" id="one-piece" href='#'>One Piece</a>
+                            <img class="img" src="./assets/photos/OnePiece.jpg" alt="OnePiece">
+                        </li>
+
+                        <li class="li">
+                            <a class="a" id="naruto" href='#'>Naruto</a>
+                            <img class="img" src="./assets/photos/Naruto.jpg" alt="Naruto">
+                        </li>
+                        </div>
+                        <div class="tab">
+                        <li class="li">
+                            <a class="a" id="dragon-ball" href='#'>Dragon Ball Z</a>
+                            <img class="img" src="./assets/photos/DragonBallZ.jpg" alt="DragonBallZ">
+                        </li>
+
+                        <li class="li">
+                            <a class="a" href='#'>Animés</a>
+                            <img class="img" src="./assets/photos/animes.jpg" alt="Animes">
+                        </li>
+                        </div>
                     </ul>
                 </div>
-                <div class="img-animes"></div>
+
+                <div class="img-animes" alt="imgAnimes">
+
+                </div>
             </div>
+
             <button class="btn" href="#">JEUX VIDEO</button>
             <div class="position">
                 <div class="cinquante-text">
-                    <ul>
-                        <li class="li"><a class="a" href='#'>Nintendo</a></li>
-                        <li class="li"><a class="a" href='#'>Jeux Rétro</a></li>
-                        <li class="li"><a class="a" href='#'>Jeux Indépendants</a></li>
-                        <li class="li"><a class="a" href='#'>Jeux Vidéo</a></li>
+                    <ul class="ul">
+                        <div class="tab">
+                        <li class="li">
+                            <a class="a" id="nintendo" href='#'>Nintendo</a>
+                            <img class="img" src="./assets/photos/nintendo.jpg" alt="nintendo">
+                        </li>
+
+                        <li class="li">
+                            <a class="a" id="jeux-video" href='#'>Jeux Rétro</a>
+                         <img class="img" src="./assets/photos/jeuxvideoretro.jpg" alt="jeuxvideo">
+                        </li>
+                        </div>
+                        <div class="tab">
+                        <li class="li">
+                            <a class="a" id="jeux-independants" href='#'>Jeux Indépendants</a>
+                            <img class="img" src="./assets/photos/jeuxvideoindependant.jpg" alt="jeuxIndependants">
+                        </li>
+
+                        <li class="li">
+                            <a class="a" href='#'>Jeux Vidéo</a>
+                        <img class="img" src="./assets/photos/Jeuxvideo2.jpg" alt="Jeuxvideo">
+                        </li>
+                         </div>
                     </ul>
                 </div>
-                <div class="img-jeuxVideo"></div>
+
+                <div class="img-jeuxVideo" alt="imgjeuxvideo">
+
+                </div>
             </div>
+
             <button class="btn" href="#">CULTURE</button>
             <div class="position">
                 <div class="cinquante-text">
-                    <ul>
-                        <li class="li"><a class="a" href='#'>Culture Pub</a></li>
-                        <li class="li"><a class="a" href='#'>Culture Musicale</a></li>
-                        <li class="li"><a class="a" href='#'>Culture Stars</a></li>
-                        <li class="li"><a class="a" href='#'>Culture</a></li>
+                    <ul class="ul">
+                        <div class="tab">
+                        <li class="li">
+                            <a class="a" id="culture-pub" href='#'>Culture Pub</a>
+                            <img class="img" src="./assets/photos/Culture-Pub.jpg" alt="culturepub">
+                        </li>
+
+                        <li class="li">
+                            <a class="a" id="culture-musicale" href='#'>Culture Musicale</a>
+                            <img class="img" src="./assets/photos/CulturreMusicale.jpg" alt="culturemusical">
+                        </li>
+                        </div>
+                        <div class="tab">
+                        <li class="li">
+                            <a class="a" id="culture-stars" href='#'>Culture Stars</a>
+                            <img class="img" src="./assets/photos/culturestars.jpg" alt="cultureStars">
+                        </li>
+
+                        <li class="li">
+                            <a class="a" href='#'>Culture</a>
+                            <img class="img" src="./assets/photos/culture.jpg" alt="culture">
+                        </li>
+                        </div>
                     </ul>
                 </div>
-                <div class="img-culture"></div>
+                <div class="img-culture" alt="imgculture"></div>
             </div>
 
 
