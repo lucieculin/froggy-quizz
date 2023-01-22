b7dfd550f48f568f138f051deea6894d9832c319
Anthony Pilenko <spirit.life@gmx.com>
2023-01-22 16:48:05 +0100

    UserRepository

---
+++ App/repository/UserRepository.php
@@ -0,0 +1,26 @@
+<?php
+
+class UserRepository
+{
+
+    private PDO $pdo;
+
+    private string $url = 'mysql:host=127.0.0.1:3306;dbname=froggy_quiz';
+    private string $user = 'root';
+    private string $pwd = '';
+
+    public function __construct()
+    {
+        $this->pdo = new PDO($this->url, $this->user, $this->pwd);
+    }
+
+
+
+
+
+
+
+
+
+
+}
